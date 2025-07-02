bef465d302e9201ed182a5792a651a34deeee2d0
Jan Janssen <janssen@mpie.de>
2025-07-02 14:23:37 +0200

    Use black formatting
    Black https://black.readthedocs.io helps to improve the readability of python code

--- helper_functions.py
+++ helper_functions.py
@@ -20,18 +20,36 @@ def train_test_split(structure_series, test_fraction=0.5):
     last_index = 0
     config_to_rows_map = []
     for i in range(n_configs):
-        config_to_rows_map.append([last_index+j for j in range(1+3*len(structure_series.values[i]))])
-        last_index += 1+3*len(structure_series.values[i])
+        config_to_rows_map.append(
+            [last_index + j for j in range(1 + 3 * len(structure_series.values[i]))]
+        )
+        last_index += 1 + 3 * len(structure_series.values[i])
     config_idxs_shuffled = [i for i in range(n_configs)]
     random.seed(58)
     random.shuffle(config_idxs_shuffled)
-    train_test_split_idx = int(len(config_idxs_shuffled) * (1-test_fraction))
-    train_idxs = [item for sublist_index in config_idxs_shuffled[:train_test_split_idx] for item in config_to_rows_map[sublist_index]]
-    test_idxs = [item for sublist_index in config_idxs_shuffled[train_test_split_idx:] for item in config_to_rows_map[sublist_index]]
+    train_test_split_idx = int(len(config_idxs_shuffled) * (1 - test_fraction))
+    train_idxs = [
+        item
+        for sublist_index in config_idxs_shuffled[:train_test_split_idx]
+        for item in config_to_rows_map[sublist_index]
+    ]
+    test_idxs = [
+        item
+        for sublist_index in config_idxs_shuffled[train_test_split_idx:]
+        for item in config_to_rows_map[sublist_index]
+    ]
 
     energy_idxs = [i[0] for i in config_to_rows_map]
     energy_mask = np.zeros(last_index, dtype=bool)
     energy_mask[energy_idxs] = True
     force_mask = ~energy_mask
 
-    return train_idxs, test_idxs, energy_mask, force_mask, train_test_split_idx, config_idxs_shuffled, config_to_rows_map
+    return (
+        train_idxs,
+        test_idxs,
+        energy_mask,
+        force_mask,
+        train_test_split_idx,
+        config_idxs_shuffled,
+        config_to_rows_map,
+    )
--- perform_one_fit.py
+++ perform_one_fit.py
@@ -12,22 +12,42 @@ eweight = int(sys.argv[4])
 file_name_structures = "data/Be_structures.h5"
 file_name_energies = "data/Be_prec_" + str(precision_to_train_on) + ".h5"
 df_structures, df_energies = load_files(file_name_structures, file_name_energies)
-train_idxs, test_idxs, energy_mask, force_mask = train_test_split(df_structures["ASEatoms"])[:4]
+train_idxs, test_idxs, energy_mask, force_mask = train_test_split(
+    df_structures["ASEatoms"]
+)[:4]
 
-aw = np.load('data/aw_' + str(twojmax) + '.npy')
-bw_train = np.load('data/bw_' + str(precision_to_train_on) + '.npy')
-bw_errors = np.load('data/bw_' + str(precision_to_calc_errors_on) + '.npy')
+aw = np.load("data/aw_" + str(twojmax) + ".npy")
+bw_train = np.load("data/bw_" + str(precision_to_train_on) + ".npy")
+bw_errors = np.load("data/bw_" + str(precision_to_calc_errors_on) + ".npy")
 
 start_time = time.time()
 aw[energy_mask] *= eweight
 bw_train[energy_mask] *= eweight
 coeffs, *_ = lstsq(aw[train_idxs], bw_train[train_idxs], 1.0e-13)
 aw[energy_mask] /= eweight
-print("Fitting finished in", time.time()-start_time, "sec")
-residual = np.dot(aw,coeffs) - bw_errors
-print("Energy training RMSE is", np.sqrt(np.mean(np.square(residual[train_idxs][energy_mask[train_idxs]]))))
-print("Force training RMSE is", np.sqrt(np.mean(np.square(residual[train_idxs][force_mask[train_idxs]]))))
-print("Energy testing RMSE is", np.sqrt(np.mean(np.square(residual[test_idxs][energy_mask[test_idxs]]))))
-print("Force testing RMSE is", np.sqrt(np.mean(np.square(residual[test_idxs][force_mask[test_idxs]]))))
-print("Energy RMSE if tested on the entire dataset is", np.sqrt(np.mean(np.square(residual[energy_mask]))))
-print("Force RMSE if tested on the entire dataset is", np.sqrt(np.mean(np.square(residual[force_mask]))))
+print("Fitting finished in", time.time() - start_time, "sec")
+residual = np.dot(aw, coeffs) - bw_errors
+print(
+    "Energy training RMSE is",
+    np.sqrt(np.mean(np.square(residual[train_idxs][energy_mask[train_idxs]]))),
+)
+print(
+    "Force training RMSE is",
+    np.sqrt(np.mean(np.square(residual[train_idxs][force_mask[train_idxs]]))),
+)
+print(
+    "Energy testing RMSE is",
+    np.sqrt(np.mean(np.square(residual[test_idxs][energy_mask[test_idxs]]))),
+)
+print(
+    "Force testing RMSE is",
+    np.sqrt(np.mean(np.square(residual[test_idxs][force_mask[test_idxs]]))),
+)
+print(
+    "Energy RMSE if tested on the entire dataset is",
+    np.sqrt(np.mean(np.square(residual[energy_mask]))),
+)
+print(
+    "Force RMSE if tested on the entire dataset is",
+    np.sqrt(np.mean(np.square(residual[force_mask]))),
+)
